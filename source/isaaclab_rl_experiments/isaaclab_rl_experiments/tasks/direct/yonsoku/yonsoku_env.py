e7d14e08a6787f39a5e881b879de27650db1ba58
anh0001 <anhrisn@gmail.com>
2025-05-06 12:00:18 +0900

    fix: Update feet names in Yonsoku environment for consistency

--- source/isaaclab_rl_experiments/isaaclab_rl_experiments/tasks/direct/yonsoku/yonsoku_env.py
+++ source/isaaclab_rl_experiments/isaaclab_rl_experiments/tasks/direct/yonsoku/yonsoku_env.py
@@ -53,7 +53,7 @@ class YonsokuEnv(DirectRLEnv):
         )
         
         # Initialize feet contact buffers
-        self.feet_names = ["RF3", "RB3", "LB3", "LF3"]  # Last link in each leg
+        self.feet_names = ["RF_FOOT", "RB_FOOT", "LB_FOOT", "LF_FOOT"] # Last link in each leg
         self.feet_indices = []
         for name in self.feet_names:
             self.feet_indices.append(self.robot.find_bodies(name)[0])
